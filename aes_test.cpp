d3768745922bbd7b9fa438818a0b5ce9a78c51e3
Mike Neri <mn.mikeneri@gmail.com>
2021-12-21 17:26:45 -0500

    testing el gamal encrypt

--- aes_test.cpp
+++ aes_test.cpp
@@ -4,9 +4,12 @@
  * Terminal command using the 2 libraries:
  * g++ -g -O2 -std=c++11 -pthread -march=native foo.cpp -o foo -lntl -lgmp -lm -L/usr/local/lib/ -lssl -lcrypto
  *
+ * g++ -g -O2 -std=c++11 foo.cpp -o foo -lssl -lcrypto -lntl -pthread -lgmp
+ *
  * This is the c++ version of the examples.c
  */
 #include <NTL/ZZ.h>
+#include <NTL/ZZ_pX.h>
 #include <openssl/aes.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
@@ -18,9 +21,80 @@
 #include <stdio.h>
 #include <cstring>
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 using namespace NTL;
+
+/* storage for parameters: 
+ * need these parameters for El Gamal*/
+ZZ q,p,g;
+
+/* We will be using the same readParams() provided to us from ntl-examples.cpp
+ * Values are placed as before: q, p, g (in this order)
+ * Values are stored in the file ./params-q-p-g
+ * NOTE: Currently, the numbers are written in base 10
+ *
+ * Of course, we also just store the numbers in this file as well for ease
+ * of testing
+ * */
+
+int readParams()
+{
+	ifstream fin("params-q-p-g");
+	if (!fin.good()) {
+		fprintf(stderr, "couldn't open parameter file.\n");
+		return 1;
+	}
+
+	/* NOTE: q,p,g have been declared above at global scope*/
+
+	fin >> q >> p >> g;
+	fin.close();
+
+	/* Let's perform a quick sanity check: are values which are
+	 * suppoed to be prime actuall so?  Does q in fact divide
+	 * the order of the multiplicative group mod p? */
+	if (!ProbPrime(q)) {
+		fprintf(stderr, "q not prime!\n");
+		return -1;
+	}
+	if (!ProbPrime(p)) {
+                fprintf(stderr, "p not prime!\n");
+                return -1;
+        }
+        if ((p-1)%q != 0) {
+                fprintf(stderr, "q does not divde p-1!\n");
+                return -1;
+        }
+	if ((p-1)%(q*q) == 0) {
+                fprintf(stderr, "q^2 divides p-1!\n");
+                return -1;
+        }
+        /* lastly, let's check on the generator: */
+        if (PowerMod(g,(p-1)/q,p) == 1) {
+                fprintf(stderr, "g does not generate subgroup of size q!\n");
+                return -1;
+        }
+	/* NOTE: we can also set the modulus for the ZZ_p datatypes.
+         * This way you never have to perform any explicit reductions
+         * when doing arithmetic, e.g., X*Y means X*Y % p for X,Y of
+         * type ZZ_p. */
+        ZZ_p::init(p);
+        /* NOTE: for secret sharing, you could also use this, but you
+         * will likely want to use parameter q instead. */
+        return 0;
+}
+
+
+/* Use later for initialization of NTL's internal RNG.
+void initNTLRandom()
+{
+        FILE* frand = fopen("/dev/urandom","rb");
+        unsigned char seed[32];
+        fread(seed,1,32,frand);
+        fclose(frand);
+        SetSeed(seed,32);
+}*/
 
 /* demonstrates HMAC */
 // HMAC(IV|C)
@@ -86,12 +160,86 @@ void aes_decrypt(unsigned char* aes_key, unsigned char* iv, unsigned char* ct,un
 	 * decrypting needs to know the IV) */
 }
 
+unsigned int el_gamal_encrypt(ZZ ZZ_x, ZZ ZZ_b){
+	cout << "EL GAMAL ENCRYTING ... " << endl;
+
+	// Need to generate secret key random a. 
+	// SK = a in {1, ..., p-1}
+	// Need to generate public key
+	// PK = A = g^a
+	
+	// Generate random a
+	
+	unsigned int p_convert;
+	p_convert = conv<uint>(p);
+	cout << "p converted = " << p_convert << endl;
+	ZZ ZZ_a = conv<ZZ>(RandomBnd(p_convert));
+
+
+	// Generate Public Key
+	unsigned int A;
+	ZZ ZZ_A;
+	ZZ_A = power(g, conv<uint>(ZZ_a));
+	A = conv<uint>(ZZ_A);
+
+	// Now we encrypt, E_pk(x) = (B, s * x)
+	// Where s is the shared secret
+	
+	// Generate the shared secret
+	
+	ZZ ZZ_s;
+	ZZ_s = power(ZZ_A, conv<uint>(ZZ_b));
+	ZZ_s = ZZ_s * ZZ_x;
+
+	return conv<uint>(ZZ_s);
+}
+
+// This is just hash function SHA-256 on value x
+int h(ZZ ZZ_x){
+
+	return 0;
+}
 
 int main()
 {
+	// Need to generate random value: x
+	// for El Gamal and Hash function, SHA-256
+
+	readParams();
+
+	ZZ ZZ_x;
+	ZZ_x = RandomBnd(10000000);
+	
+	cout << "Random number, x: " << ZZ_x << endl;
+	cout << "q = " << q << endl;
+	cout << "p = " << p << endl;
+	cout << "g = " << g << endl;
+	
+	// El Gamal using Public Key Encryption
+
+	// Generate random b, we need b later for decryption
+
+	unsigned int p_convert = conv<uint>(p);
+	cout << "p converted = " << p_convert << endl;
+	ZZ ZZ_b = conv<ZZ>(RandomBnd(p_convert));
+	
+	// Generate B, using b.
+	// May run out of memory here!
+	ZZ ZZ_B = power(g, conv<uint>(ZZ_b));
+	
+	unsigned int E_Pk_x;
+	E_Pk_x = el_gamal_encrypt(ZZ_x,ZZ_b);
+
+	cout << "E_Pk_x:" << endl;
+        cout << "B = " << ZZ_B << endl;
+	cout << "s = " << E_Pk_x << endl;	
+
+	// SHA-256 hash of x
+	h(ZZ_x);
+
 	unsigned char aes_key[32]; // aes_key is 32 bytes
-	size_t i;
 	// needs to be iv and key must be random(use NTL random)
+	size_t i;
 	for (i = 0; i < 32; i++)
 	{
 		size_t rng = RandomBnd(93) + 33; // From ASCII table, usable chars are from dec 33 - 126
